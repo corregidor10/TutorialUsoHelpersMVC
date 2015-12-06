5a091566ea558014aa488f5f4bd0003bfe648045
Daniel Corregidor Coronado <corregidor10@hotmail.com>
2015-12-06 13:29:54 +0100

    firstCommit

---
+++ DemoPlantillaRM/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoPlantillaRM.Models;
+
+namespace DemoPlantillaRM.Controllers
+{
+    public class HomeController : Controller
+    {
+        JugadoresEntities db= new JugadoresEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            
+            return View(db.Plantilla);
+        }
+
+        public ActionResult Alta()
+        {
+            return View(new Plantilla());
+        }
+
+        [HttpPost]
+        public ActionResult Alta(Plantilla model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                db.Plantilla.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+    }
+}
